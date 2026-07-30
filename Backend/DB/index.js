7caefc03516227205346e91f1fac70350b016e7c
Arpan Banik <arpanbanik976@gmail.com>
2026-07-30 11:27:00 +0530

    Add new code base

--- Backend/DB/index.js
+++ Backend/DB/index.js
@@ -1,5 +1,10 @@
+import dns from "dns";
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js";
+
+
 
 const connectDB= async()=>{
     try {
